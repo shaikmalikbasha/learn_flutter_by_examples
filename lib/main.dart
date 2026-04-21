dcb379317440505359143599b9a5586f6bc9ed0c
shaikmalikbasha <shaikmalikbasha@gmail.com>
2026-04-21 11:28:42 +0530

    DiceApp Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,92 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:learn_flutter_by_examples/utils/constants.dart';
+
+String getRandomImageUrl() {
+  final images = <String>[
+    'images/dice1.png',
+    'images/dice2.png',
+    'images/dice3.png',
+    'images/dice4.png',
+    'images/dice5.png',
+    'images/dice6.png',
+  ];
+
+  Random random = Random();
+  return images[random.nextInt(images.length)];
+}
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(const DiceApp());
+}
+
+class DiceApp extends StatelessWidget {
+  const DiceApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'I am Rich',
+      title: appName,
       home: Scaffold(
-        backgroundColor: Colors.blue[200],
+        backgroundColor: Colors.red.shade900,
         appBar: AppBar(
-          backgroundColor: Colors.blueGrey[900],
-          title: Text('I am Rich'),
+          elevation: 4.0,
+          shadowColor: Colors.black,
+          backgroundColor: Colors.red.shade900,
+          title: Text(
+            appName,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 3.0,
+            ),
+          ),
         ),
-        body: Center(child: Image(image: AssetImage('images/school-icon.png'))),
+        body: const DicePage(),
       ),
-    ),
-  );
+    );
+  }
+}
+
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  String leftDiceImage = 'images/dice1.png';
+  String rightDiceImage = 'images/dice1.png';
+
+  void rollBothDice() {
+    setState(() {
+      leftDiceImage = getRandomImageUrl();
+      rightDiceImage = getRandomImageUrl();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+              onPressed: () => rollBothDice(),
+              child: Image.asset(leftDiceImage),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () => rollBothDice(),
+              child: Image.asset(rightDiceImage),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
